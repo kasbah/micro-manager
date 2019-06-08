1197be04e9f7d9aa8f5268fc59e66ce3b85738fb
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-06-08 16:12:07 +0200

    Start HiSeq2x00Camera adapter

---
+++ DeviceAdapters/HiSeq2x00Camera/HiSeq2x00Camera.cpp
@@ -0,0 +1,99 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          HiSeq2x00Camera.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Illumina HiSeq 2000 and 2500 Hamamatsu camera based on DCAM API
+// COPYRIGHT:     Kaspar Emanuel 2019
+//
+//
+// LICENSE:       This library is free software; you can redistribute it and/or
+//                modify it under the terms of the GNU Lesser General Public
+//                License as published by the Free Software Foundation.
+//
+//                You should have received a copy of the GNU Lesser General Public
+//                License along with the source distribution; if not, write to
+//                the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+//                Boston, MA  02111-1307  USA
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+// AUTHOR:        Kaspar Emanuel
+//
+// CVS:           $Id$
+//
+#include "HiSeq2x00Camera.h"
+
+#include <string>
+
+using namespace std;
+
+const char* g_DeviceName = "HiSeq2x00Camera";
+
+CHiSeq2x00Camera::CHiSeq2x00Camera()
+{}
+
+CHiSeq2x00Camera::~CHiSeq2x00Camera()
+{}
+
+void CHiSeq2x00Camera::GetName(char* name) const
+{
+	CDeviceUtils::CopyLimitedString(name, g_DeviceName);
+}
+
+int CHiSeq2x00Camera::Initialize()
+{
+	return DEVICE_OK;
+}
+
+int CHiSeq2x00Camera::Shutdown()
+{
+	return DEVICE_OK;
+}
+
+
+bool CHiSeq2x00Camera::Busy(){
+	return false;
+}
+
+
+int CHiSeq2x00Camera::SnapImage()
+{
+	return DEVICE_OK;
+}
+
+
+const unsigned char* CHiSeq2x00Camera::GetImageBuffer()
+{
+
+	return (unsigned char*)0;
+}
+
+MODULE_API void InitializeModuleData()
+{
+	// AddAvailableDeviceName(g_DeviceName, "Universal adapter using DCAM interface");
+	RegisterDevice(g_DeviceName, MM::CameraDevice, "HiSeq 2000 and 2500 camera DCAM interface");
+}
+
+MODULE_API void DeleteDevice(MM::Device* pDevice)
+{
+	delete pDevice;
+}
+
+MODULE_API MM::Device* CreateDevice(const char* deviceName)
+{
+	if (deviceName == 0)
+		return 0;
+
+	string strName(deviceName);
+
+	if (strcmp(deviceName, g_DeviceName) == 0)
+		return new CHiSeq2x00Camera();
+
+	return 0;
+}
---
+++ DeviceAdapters/HiSeq2x00Camera/HiSeq2x00Camera.h
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          HiSeq2x00Camera.h
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Illumina HiSeq 2000 and 2500 Hamamatsu camera based on DCAM API
+// COPYRIGHT:     Kaspar Emanuel 2019
+//
+//
+// LICENSE:       This library is free software; you can redistribute it and/or
+//                modify it under the terms of the GNU Lesser General Public
+//                License as published by the Free Software Foundation.
+//
+//                You should have received a copy of the GNU Lesser General Public
+//                License along with the source distribution; if not, write to
+//                the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+//                Boston, MA  02111-1307  USA
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+// AUTHOR:        Kaspar Emanuel
+//
+// CVS:           $Id$
+//
+
+#ifndef _HISEQ2X00CAMERA_H_
+#define _HISEQ2X00CAMERA_H_
+
+#include "../../MMDevice/DeviceBase.h"
+#include "../../MMDevice/ImgBuffer.h"
+
+class CHiSeq2x00Camera : public CCameraBase<CHiSeq2x00Camera>
+{
+public:
+	CHiSeq2x00Camera();
+	~CHiSeq2x00Camera();
+	// MMDevice API
+	int Initialize();
+	int Shutdown();
+
+	void GetName(char* pszName) const;
+	bool Busy();
+
+	// MMCamera API
+	int SnapImage();
+	const unsigned char* GetImageBuffer();
+	unsigned GetImageWidth() const { return img_.Width(); }
+	unsigned GetImageHeight() const { return img_.Height(); }
+	unsigned GetImageBytesPerPixel() const { return img_.Depth(); }
+	long GetImageBufferSize() const { return img_.Width() * img_.Height() * GetImageBytesPerPixel(); }
+	unsigned GetBitDepth() const { return 0; };
+	double GetExposure() const { return 0; };
+	void SetExposure(double dExp) {};
+	int SetROI(unsigned uX, unsigned uY, unsigned uXSize, unsigned uYSize) {
+		return DEVICE_OK;
+	};
+	int GetROI(unsigned& uX, unsigned& uY, unsigned& uXSize, unsigned& uYSize) {
+		return DEVICE_OK;
+	};
+	int ClearROI() {
+		return DEVICE_OK;
+	};
+	int GetBinning() const { return lnBin_; };
+	int SetBinning(int binSize) {
+		return DEVICE_OK;
+	};
+
+
+	// high-speed interface
+	int StartSequenceAcquisition(long numImages, double interval_ms, bool stopOnOverflow) {
+		return DEVICE_OK;
+	};
+	int StopSequenceAcquisition() {
+		return DEVICE_OK;
+	};
+	int RestartSequenceAcquisition() {
+		return DEVICE_OK;
+	};
+	bool IsCapturing() {
+		return false ;
+	};
+	int RestartSnapMode() {
+		return DEVICE_OK;
+	};
+	int IsExposureSequenceable(bool& isSequenceable) const { isSequenceable = false; return DEVICE_OK; }
+
+
+	// custom interface for the burst thread
+	int PushImage();
+private:
+	ImgBuffer img_;
+	long lnBin_;
+};
+
+
+#endif //_HISEQ2X00CAMERA_H_
