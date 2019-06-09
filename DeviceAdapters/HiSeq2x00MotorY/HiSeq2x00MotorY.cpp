91bea673f3788890b1ee23b2a01d904e2abb0206
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-06-09 17:28:20 +0200

    Start HiSeq2x00MotorY

---
+++ DeviceAdapters/HiSeq2x00MotorY/HiSeq2x00MotorY.cpp
@@ -0,0 +1,87 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          HiSeq2x00MotorY.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Illumina HiSeq 2000 and 2500 Y motor stage adapter
+// COPYRIGHT:     Kaspar Emanuel 2019
+//
+//
+// LICENSE:       This library is free software; you can redistribute it and/or
+//                modify it under the terms of the GNU Lesser General Public
+//                License as published by the Free Software Foundation.
+//
+//                You should have received a copy of the GNU Lesser General Public
+//                License along with the source distribution; if not, write to
+//                the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+//                Boston, MA  02111-1307  USA
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+// AUTHOR:        Kaspar Emanuel
+//
+// CVS:           $Id$
+//
+#include "HiSeq2x00MotorY.h"
+
+#include <string>
+
+using namespace std;
+
+const char* g_DeviceName = "HiSeq2x00MotorY";
+
+CHiSeq2x00MotorY::CHiSeq2x00MotorY()
+{}
+
+CHiSeq2x00MotorY::~CHiSeq2x00MotorY()
+{}
+
+void CHiSeq2x00MotorY::GetName(char* name) const
+{
+	CDeviceUtils::CopyLimitedString(name, g_DeviceName);
+}
+
+int CHiSeq2x00MotorY::Initialize()
+{
+	int ret = DEVICE_OK;
+	return ret;
+}
+
+int CHiSeq2x00MotorY::Shutdown()
+{
+	return DEVICE_OK;
+}
+
+
+bool CHiSeq2x00MotorY::Busy(){
+	return false;
+}
+
+
+MODULE_API void InitializeModuleData()
+{
+	RegisterDevice(g_DeviceName, MM::CameraDevice, "HiSeq 2000 and 2500 Y motor adapter");
+}
+
+MODULE_API void DeleteDevice(MM::Device* pDevice)
+{
+	delete pDevice;
+}
+
+MODULE_API MM::Device* CreateDevice(const char* deviceName)
+{
+	if (deviceName == 0)
+		return 0;
+
+	string strName(deviceName);
+
+	if (strcmp(deviceName, g_DeviceName) == 0)
+		return new CHiSeq2x00MotorY();
+
+	return 0;
+}
---
+++ DeviceAdapters/HiSeq2x00MotorY/HiSeq2x00MotorY.h
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          HiSeq2x00MotorY.h
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Illumina HiSeq 2000 and 2500 Y motor stage adapter
+// COPYRIGHT:     Kaspar Emanuel 2019
+//
+//
+// LICENSE:       This library is free software; you can redistribute it and/or
+//                modify it under the terms of the GNU Lesser General Public
+//                License as published by the Free Software Foundation.
+//
+//                You should have received a copy of the GNU Lesser General Public
+//                License along with the source distribution; if not, write to
+//                the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+//                Boston, MA  02111-1307  USA
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+// AUTHOR:        Kaspar Emanuel
+//
+// CVS:           $Id$
+//
+
+#ifndef _HISEQ2X00MOTORY_H_
+#define _HISEQ2X00MOTORY_H_
+
+#include "../../MMDevice/DeviceBase.h"
+
+class CHiSeq2x00MotorY : public CStageBase<CHiSeq2x00MotorY>
+{
+public:
+	CHiSeq2x00MotorY();
+	~CHiSeq2x00MotorY();
+	// MMDevice API
+	int Initialize();
+	int Shutdown();
+
+	void GetName(char* name) const;
+	bool Busy();
+
+	int SetPositionUm(double pos) { pos_um_ = pos; return DEVICE_OK; };
+	int GetPositionUm(double& pos) { pos = pos_um_; return DEVICE_OK; };
+	int SetOrigin() { return DEVICE_OK; };
+	int GetLimits(double& min, double& max) { return DEVICE_OK; };
+	double GetStepSize() { return 0; }
+	int SetPositionSteps(long steps) { return DEVICE_OK; };
+	int GetPositionSteps(long& steps) { return DEVICE_OK; };
+	int IsStageSequenceable(bool& isSequenceable) const { isSequenceable = false; return DEVICE_OK; }
+	bool IsContinuousFocusDrive(void) const { return false; };
+
+private:
+	double pos_um_;
+
+};
+
+
+#endif //_HISEQ2X00MOTORY_H_
